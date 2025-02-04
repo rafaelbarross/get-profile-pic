f047dba3ac12567474c38e8c65fc1dd90421d116
Rafael Barros <102997863+rafaelbarross@users.noreply.github.com>
2025-02-04 16:45:50 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import instaloader
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/get_profile_pic', methods=['GET'])
+def get_profile_pic():
+    username = request.args.get('username')
 
-@app.route('/about')
-def about():
-    return 'About'
+    if not username:
+        return jsonify({'error': 'Username parameter is missing.'}), 400
+
+    try:
+        L = instaloader.Instaloader()
+        profile = instaloader.Profile.from_username(L.context, username)
+        profile_pic_url = profile.profile_pic_url
+
+        return jsonify({'username': username, 'profile_pic_url': profile_pic_url})
+
+    except instaloader.exceptions.ProfileNotExistsException:
+        return jsonify({'error': 'User does not exist.'}), 404
+
+@app.route('/', methods=['GET'])
+def error_message():
+    return jsonify({'error': 'Invalid API endpoint. Use /get_profile_pic?username=<username>'}), 400
+
+if __name__ == '__main__':
+    app.run(debug=False, port=80)
